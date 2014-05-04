f6ef83f94e812a84357b782fef9d3720f3e2cee0
“ahmet <ahmetmobile@gmail.com>
2014-05-04 19:33:54 -0400

    degisiklik

--- class and definitation examples Araba porsche.rb
+++ class and definitation examples Araba porsche.rb
@@ -1,45 +1,62 @@
-class TheCar
-  attr_accessor :brand, :model, :year,:hours_power, :max_speed, :owner_name
+=begin
+# class TheCar
+#   attr_accessor :brand, :model, :year,:hours_power, :max_speed, :owner_name
+#
+# end
+#
+# def car_screen
+#   puts "Brand: #{@cars.brand.capitalize}"
+#   puts "Model: #{@cars.model}"
+#   puts "Year: #{@cars.year}"
+#   puts "HP: #{@cars.hours_power}"
+#   puts "Max Speed: #{@cars.max_speed}"
+#   puts "Owner : #{@cars.owner_name.upcase}"
+# end
+#
+# @cars = TheCar.new
+#
+# loop do
+# print "\n What is the brand? : "
+# @cars.brand = gets.chomp
+# print "What is the model? : "
+# @cars.model = gets.chomp.to_s
+# print "What year made? : "
+# @cars.year = gets.chomp.to_i
+# print "How many hours power? : "
+# @cars.hours_power = gets.chomp.to_i
+# print "What is the maximum speed? : "
+# @cars.max_speed = gets.chomp.to_i
+# print "Who is the current owner? : "
+# @cars.owner_name = gets.chomp.to_s
+#
+# puts  "Would you like to see the information about the car"
+# puts "Use 'Y' for YES"
+#
+# if gets.chomp.downcase == 'y'
+#   car_screen
+# else
+#   puts
+#   puts "Thank you for your entry"
+#   puts
+# end
+#
+#
+# end
+#
+=end
+
+
+
+
+class CustomersClass
+  attr_accessor :userID, :user_name, :user_location, :user_job
+end
+
+def
 
 end
 
-def car_screen
-  puts "Brand: #{@cars.brand.capitalize}"
-  puts "Model: #{@cars.model}"
-  puts "Year: #{@cars.year}"
-  puts "HP: #{@cars.hours_power}"
-  puts "Max Speed: #{@cars.max_speed}"
-  puts "Owner : #{@cars.owner_name.upcase}"
-end
+customers = CustomersClass.new
 
-@cars = TheCar.new
-
-loop do
-print "\n What is the brand? : "
-@cars.brand = gets.chomp
-print "What is the model? : "
-@cars.model = gets.chomp.to_s
-print "What year made? : "
-@cars.year = gets.chomp.to_i
-print "How many hours power? : "
-@cars.hours_power = gets.chomp.to_i
-print "What is the maximum speed? : "
-@cars.max_speed = gets.chomp.to_i
-print "Who is the current owner? : "
-@cars.owner_name = gets.chomp.to_s
-
-puts  "Would you like to see the information about the car"
-puts "Use 'Y' for YES"
-
-if gets.chomp.downcase == 'y'
-  car_screen
-else
-  puts
-  puts "Thank you for your entry"
-  puts
-end
-
-
-end
-
-
+puts  "Please enter user name "
+customers.userID = gets.chomp
