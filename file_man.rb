21752fec420c4fc4d051d6b8eea94a09867b386f
“ahmet <ahmetmobile@gmail.com>
2014-05-10 05:48:46 -0400

    new submenu travel feature
    added loop function to travel in menus.

--- file_man.rb
+++ file_man.rb
@@ -22,18 +22,21 @@ end
 
 def multi_question_answering
 
+  puts "*********** FILE MANAGER ************"
   puts "What would you like to do ? "
   puts "To Create New File Enter '1' "
   puts "To Open a Existing File '2'"
   puts "To Delete a Existing File '3'"
+  puts "*************************************"
   print "> "
 end
 
 def return_back_or_read_answering
 
   puts "What would you like to do now ? "
-  puts "To Read the file you entered  '1' "
-  puts "Go to Main Menu '2'"
+  puts "To Read the file you entered  1 "
+  puts "To Return to previous menu 2 "
+  puts "Go to Main Menu 3"
   print "> "
 end
 
@@ -79,11 +82,15 @@ end
 
 menu_repeater = true
 
+
+
 while menu_repeater
   puts `clear`
+  sub_menu_repeater = true
   multi_question_answering
   question_answering
 
+  while sub_menu_repeater
     if @answer == '1'
 
       file_name_entry
@@ -92,52 +99,137 @@ while menu_repeater
       puts "The date entered into file"
       return_back_or_read_answering
       question_answering
-
         if @answer == '1'
           open_a_file_function
-          puts
-          return_back_or_read_answering
-          question_answering
-
+          puts "Return to main menu press any key"
+          gets
+          sub_menu_repeater = false
         elsif @answer == '2'
-          menu_repeater
+          sub_menu_repeater = false
+
         end
+
+
 
     elsif @answer == '2'
       file_name_entry
       open_a_file_function
       return_back_or_read_answering
       question_answering
-        if @answer == '1'
-          open_a_file_function
-          return_back_or_read_answering
-          question_answering
-            if @answer == '1'
-              open_a_file_function
-              return_back_or_read_answering
-              question_answering
-            elsif @answer == '1'
-              menu_repeater
-            end
+      if @answer == '1'
+        open_a_file_function
+        puts "Return to main menu press any key"
+        gets
+        sub_menu_repeater = false
+      elsif @answer == '2'
+        sub_menu_repeater = false
 
-        elsif @answer == '2'
-          menu_repeater
-        end
+      end
 
 
     elsif @answer == '3'
-
       file_name_entry
       delete_a_file_with_function
       return_back_or_delete_answering
       question_answering
+      if @answer == '1'
+        file_name_entry
+        delete_a_file_with_function
+        puts "Return to main menu press any key"
+        gets
+        sub_menu_repeater = false
+      elsif @answer == '2'
+        sub_menu_repeater = false
 
-        if @answer == '1'
+      end
 
-
-        elsif
-          menu_repeater
-        end
-
+    end
   end
+
 end
+
+
+
+# =begin
+#
+# menu_repeater = true
+#
+# while menu_repeater
+#   #puts `clear`
+#   multi_question_answering
+#   question_answering
+#
+#     if @answer == '1'
+#
+#       file_name_entry
+#       file_create
+#       write_on_file_function
+#       puts "The date entered into file"
+#       return_back_or_read_answering
+#       question_answering
+#
+#       repeat_open_file = true
+#       while repeat_open_file
+#
+#         if @answer == '1'
+#           open_a_file_function
+#           puts
+#           return_back_or_read_answering
+#           question_answering
+#                 if @answer = '1'
+#
+#                 end
+#
+# end
+#
+#
+#         elsif @answer == '2'
+#       end
+#     end
+#
+#
+#
+#
+#     elsif @answer == '2'
+#       file_name_entry
+#       open_a_file_function
+#       return_back_or_read_answering
+#       question_answering
+#         if @answer == '1'
+#           open_a_file_function
+#           return_back_or_read_answering
+#           question_answering
+#               if @answer == '1'
+#               # open the file again
+#               open_a_file_function
+#
+#               elsif @answer == '2'
+#               puts
+#
+#               elsif @answer == '3'
+#
+#               puts
+#
+#               end
+#
+#
+#         elsif @answer == '2'
+#           puts
+#
+#         end
+#
+#     elsif @answer == '3'
+#
+#       file_name_entry
+#       delete_a_file_with_function
+#       return_back_or_delete_answering
+#       question_answering
+#
+#         if @answer == '1'
+#
+#
+#         elsif
+#          puts
+#
+#         end
+#=end
